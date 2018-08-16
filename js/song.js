a234d85f12648d31fb9937c0a65f5548da8143ba
Aininin <Aininin7@163.com>
2018-08-16 15:29:24 +0800

    lyric

--- js/song.js
+++ js/song.js
@@ -2,7 +2,7 @@ $(function(){
 	let id = parseInt(location.search.match(/\bid=([^$]*)/)[1]);
 
 	$.get('./songs.json').then(function(response){
-		// let songs = JSON.parse(response);
+		//let songs = JSON.parse(response);
 		let songs = response;
 		let song = songs.filter((s)=>s.id === id)[0];
 		let {url,name,singer,coverImg,bgImg,lyric} = song;
@@ -25,6 +25,34 @@ $(function(){
 			audio.play()
 			$('.disc-container').addClass('playing')
 		})
+		//当前歌词显示
+		setInterval(function(){
+			let seconds = audio.currentTime; //当前歌词秒数
+			let minutes = ~~(seconds / 60); //当前歌词分钟，两次取反可以对分钟数取整
+			let left = seconds -minutes*60; //剩下的时间
+			let time = `${pad(minutes)}:${pad(left)}`; //让当前歌词时间格式与JSON里一样
+			let $lines = $('.lines > p'); //获取html中歌词列表
+			let $whichLine; //当前显示哪一行
+			for(let i=0; i<$lines.length; i++){
+				//如果歌词列表有下一行，当前行时间小于当前播放时间，下一行时间大于当前播放时间
+				if ($lines.eq(i+1).length !=0 && $lines.eq(i).attr('data-time')<time && $lines.eq(i+1).attr('data-time')>time) {
+					$whichLine = $lines.eq(i);
+					break;
+				}
+			}
+			if($whichLine){
+				$whichLine.addClass('active').prev().removeClass('active');
+				let top = $whichLine.offset().top;
+				let linesTop = $('.lines').offset().top;
+				let linesHeight = $('.lyric').height()/3;
+				let delta = top - linesTop - linesHeight;
+				$('.lines').css('transform',`translateY(-${delta}px)`);
+			}
+		},500);
+	}
+	//给播放到哪一秒补0
+	function pad(number){
+		return number >= 10? number+'' : '0'+number;
 	}
 	//初始化歌曲名，歌手,歌词,封面图片，背景图片
 	function initText(name,singer,coverImg,bgImg,lyric){
